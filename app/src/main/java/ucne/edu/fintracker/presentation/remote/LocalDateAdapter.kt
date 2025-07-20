e7fac7eb577266d64c723469767e4fbc91b786bb
Alma Ozoria <rosaalma624@gmail.com>
2025-07-20 00:09:30 -0400

    se adapto para que pueda resivir la fecha de la api

--- app/src/main/java/ucne/edu/fintracker/presentation/remote/LocalDateAdapter.kt
+++ app/src/main/java/ucne/edu/fintracker/presentation/remote/LocalDateAdapter.kt
@@ -1,21 +1,31 @@
 package ucne.edu.fintracker.presentation.remote
+
 import com.squareup.moshi.FromJson
 import com.squareup.moshi.ToJson
 import org.threeten.bp.OffsetDateTime
 import org.threeten.bp.ZoneOffset
 import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.LocalDateTime
 
 class LocalDateTimeAdapter {
     private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
 
     @ToJson
     fun toJson(dateTime: OffsetDateTime): String {
-        // Lo forzamos a UTC y lo formateamos
         return dateTime.withOffsetSameInstant(ZoneOffset.UTC).format(formatter)
     }
 
     @FromJson
     fun fromJson(json: String): OffsetDateTime {
-        return OffsetDateTime.parse(json, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        return try {
+            // Intenta parsear con offset
+            OffsetDateTime.parse(json, DateTimeFormatter.ISO_OFFSET_DATE_TIME)
+        } catch (e: Exception) {
+            // Si no tiene offset, lo asumimos UTC
+            val localFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss")
+            val localDateTime = LocalDateTime.parse(json, localFormatter)
+            localDateTime.atOffset(ZoneOffset.UTC)
+        }
     }
 }
+
