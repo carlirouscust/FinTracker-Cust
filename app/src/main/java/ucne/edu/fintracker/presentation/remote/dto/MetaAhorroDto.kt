64a0d690267c350cc25f940911072fc335781c8d
Alma Ozoria <rosaalma624@gmail.com>
2025-07-22 02:40:40 -0400

    Se modificaron para meta de ahorro

--- app/src/main/java/ucne/edu/fintracker/presentation/remote/dto/MetaAhorroDto.kt
+++ app/src/main/java/ucne/edu/fintracker/presentation/remote/dto/MetaAhorroDto.kt
@@ -1,13 +1,15 @@
 package ucne.edu.fintracker.presentation.remote.dto
 
-import org.threeten.bp.LocalDate
+import org.threeten.bp.OffsetDateTime
 
 data class MetaAhorroDto(
     val metaAhorroId: Int = 0,
     val nombreMeta: String,
     val montoObjetivo: Double,
-    val fechaFinalizacion: LocalDate,
+    val fechaFinalizacion: OffsetDateTime,
     val contribucionRecurrente: Double? = null,
     val imagen: String? = null,
-    val montoActual: Double? = null
+    val montoActual: Double? = null,
+    val montoAhorrado: Double? = null,
+    val fechaMontoAhorrado: OffsetDateTime? = null
 )
