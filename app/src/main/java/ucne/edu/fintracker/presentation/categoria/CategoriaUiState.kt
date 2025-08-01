74943d7ba7f8fb3887f4b084bf34e11fa99d862c
Carlos Manuel Custodio Then <carlos_custodio@ucne.edu.do>
2025-08-01 01:40:05 -0400

    se agrego el filtrotipo de categoria

--- app/src/main/java/ucne/edu/fintracker/presentation/categoria/CategoriaUiState.kt
+++ app/src/main/java/ucne/edu/fintracker/presentation/categoria/CategoriaUiState.kt
@@ -4,11 +4,12 @@ import ucne.edu.fintracker.presentation.remote.dto.CategoriaDto
 
 data class CategoriaUiState(
     val nombre: String = "",
-    val tipo: String = "Gasto", // o "Ingreso"
+    val tipo: String = "Gasto",
     val icono: String = "",
     val colorFondo: String = "FF0000",
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val categorias: List<CategoriaDto> = emptyList(),
+    val filtroTipo: String = "",
     val error: String? = null
 )
