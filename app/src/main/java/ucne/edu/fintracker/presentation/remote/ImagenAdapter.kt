31d7f3d534644ffc1d3793e014967cdaef2e3e28
Alma Ozoria <rosaalma624@gmail.com>
2025-08-05 22:09:28 -0400

    se creo un adaptador para img

---
+++ app/src/main/java/ucne/edu/fintracker/presentation/remote/ImagenAdapter.kt
@@ -0,0 +1,25 @@
+package ucne.edu.fintracker.presentation.remote
+
+import android.content.Context
+import android.net.Uri
+import java.io.File
+import java.io.FileOutputStream
+
+fun ImagenAdapter (context: Context, uri: Uri): String? {
+    return try {
+        val inputStream = context.contentResolver.openInputStream(uri)
+        val fileName = "meta_${System.currentTimeMillis()}.jpg"
+        val file = File(context.cacheDir, fileName)
+
+        inputStream?.use { input ->
+            FileOutputStream(file).use { output ->
+                input.copyTo(output)
+            }
+        }
+
+        file.absolutePath
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
