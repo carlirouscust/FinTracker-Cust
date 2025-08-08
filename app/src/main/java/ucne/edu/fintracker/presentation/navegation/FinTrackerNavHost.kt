454237ee942f030d0902495af8752a0bc891a26d
Alma Ozoria <rosaalma624@gmail.com>
2025-08-08 18:45:43 -0400

    se modifico

--- app/src/main/java/ucne/edu/fintracker/presentation/navegation/FinTrackerNavHost.kt
+++ app/src/main/java/ucne/edu/fintracker/presentation/navegation/FinTrackerNavHost.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
